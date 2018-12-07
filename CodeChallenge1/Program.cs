5c1c47813d3092b70ccca9a58cb569b5798b1dc5
Bruce Hinrichs <register@oaksavannasoftware.com>
2018-12-07 08:14:56 -0600

    Add project files.

---
+++ CodeChallenge1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace CodeChallenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //process into a list of numbers
+            string list = args[0];
+
+            //convert to list of numbers
+            List<int> listOfNumbers = list.Split(',').Select(int.Parse).ToList();
+            IEnumerable<int> listOfNumbers2 = list.Split(',').Select(int.Parse);
+            listOfNumbers.Sort();
+            
+            var listCount = listOfNumbers.Count();
+            Console.Write("{");
+            Console.Write(listOfNumbers[0]);
+            for (int i = 0; i < listCount-1; i++)
+            {
+                
+                if (listOfNumbers[i] + 1 == listOfNumbers[i + 1])
+                {
+                    Console.Write(",");
+                    Console.Write(listOfNumbers[i + 1]);
+                }
+                else
+                {
+                    Console.Write("}");
+                    if (i < listCount)
+                    {
+                        Console.Write(",{");
+                        Console.Write(listOfNumbers[i + 1]);
+                    }                    
+                }
+                
+            }
+            Console.Write("}");
+        }
+    }
+}
